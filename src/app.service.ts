16e98ab5f50404a28d93f029a7750837bc07dd60
Dano-19 <dam.cardenas@yavirac.edu>
2024-06-28 10:46:23 -0500

    Renombre Github

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Nota } from './entities/nota.entity';
+import { Repository  } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(Nota)
+    private notasRepository: Repository<Nota>,
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    const nota: Nota = new Nota();
+    nota.description = 'Hello world!!';
+    this.notasRepository.save(nota);
+    return 'sucess!';
   }
 }
